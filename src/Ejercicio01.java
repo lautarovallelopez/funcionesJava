492082fcbb82ba5c3e766d9e9ced711199d71d10
lautaro valle lopez <lautarovallelopez@gmail.com>
2024-02-21 15:10:28 -0300

    first commit

---
+++ src/Ejercicio01.java
@@ -0,0 +1,13 @@
+/*
+Escriba un procedimiento que escriba “Hola Mundo” cuando uno la
+invoque.
+
+* */
+public class Ejercicio01 {
+    public static void main(String[] args) {
+        saludar();
+    }
+    public static void saludar() {
+        System.out.println("Hola Mundo");
+    }
+}
